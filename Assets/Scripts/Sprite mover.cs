9ea951ae7ab5c9eb96e72a41a6b864d8095999b0
Ozzyosboune <Ozzyosboune@users.noreply.github.com>
2018-05-26 20:26:34 -0700

    first try
    here we go

---
+++ Assets/Scripts/Sprite mover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spritemover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(1f * Time.deltaTime, 0f, 0f);
+
+	}
+}
