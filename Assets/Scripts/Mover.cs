f92b314ca6298cbeae20aa719efeab3ae5c4c9b2
Ozzyosboune <Ozzyosboune@users.noreply.github.com>
2018-05-27 20:49:20 -0700

    progress
    so far so good

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -16,26 +16,53 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            tf.position -= tf.right;
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                tf.position -= tf.right;
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                tf.position += tf.right;
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                tf.position += tf.up;
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                tf.position -= tf.up;
+            }
+
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        else
         {
-            tf.position += tf.right;
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                tf.position -= tf.right;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                tf.position += tf.right;
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                tf.position += tf.up;
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                tf.position -= tf.up;
+            }
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.Space))
         {
-            tf.position += tf.up;
+            tf.position = new Vector2 (0.0f, 0.0f);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.P))
         {
-            tf.position -= tf.up;
+
         }
-
-
-
-
 
 
 
